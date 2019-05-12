a575da4c87084fc93df5f1fcde6290a7c584c192
someoneb100 <lawamulet@gmail.com>
2019-05-12 22:44:17 +0200

    Version 1.0.3

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 touch $HOME/.hushlogin
 echo "source ~/env/.profile_init" >> ~/.profile
 apt update
-apt install --assume-yes tmux git python nmap cowsay toilet vim clang
+apt install --assume-yes tmux git python nmap cowsay toilet vim clang flex bison
 cp -r env/ ~/env/
 cp gitconfig ~/.gitconfig
 exec bash
