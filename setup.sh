9c1d56849adb23ca9ae1cde8b60f0ecd0c31038e
someoneb100 <lawamulet@gmail.com>
2019-05-12 22:29:32 +0200

    Version 1.0.2

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 touch $HOME/.hushlogin
 echo "source ~/env/.profile_init" >> ~/.profile
 apt update
-apt install --assume-yes tmux git python nmap cowsay toilet
+apt install --assume-yes tmux git python nmap cowsay toilet vim
 cp -r env/ ~/env/
 cp gitconfig ~/.gitconfig
 source ~/.profile
