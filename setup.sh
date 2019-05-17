91c1e23937b97f404564a1bce5f29d8dea264907
someoneb100 <lawamulet@gmail.com>
2019-05-17 15:07:44 +0200

    Version 2.0.3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 touch ~/.hushlogin
 echo 't=$(date -r ~/env/bash/ "+%m-%d-%y-%H-%M-%S")' >> ~/.profile
-echo 'if [ "$t" -ne "$(cat ~/env/.date)" ]; then python ~/env/.prepare.py "$t"; fi' >> ~/.profile
+echo 'if [ "$t" != "$(cat ~/env/.date)" ]; then python ~/env/.prepare.py "$t"; fi' >> ~/.profile
 echo 'source ~/env/.profile_init' >> ~/.profile
 apt update
 apt install --assume-yes coreutils tmux git python nmap cowsay toilet vim clang flex bison ffmpeg pulseaudio
