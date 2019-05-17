a97a4ced20c3a1a3bd94164a90ee3ce3d535a658
someoneb100 <lawamulet@gmail.com>
2019-05-17 11:50:14 +0200

    Version 1.0.6

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 touch $HOME/.hushlogin
 echo "source ~/env/.profile_init" >> ~/.profile
 apt update
-apt install --assume-yes tmux git python nmap cowsay toilet vim clang flex bison
+apt install --assume-yes tmux git python nmap cowsay toilet vim clang flex bison ffmpeg pulseaudio
+pip install --yes yoitube-dl
 cp -r env/ ~/env/
 cp gitconfig ~/.gitconfig
 termux-setup-storage
